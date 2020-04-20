9b240f21bc4fa0e30e745aef2e004c003e28c14f
Faisal Alkheraiji <FaisalAlkheraiji@gmail.com>
2020-04-20 18:22:34 +0300

    Update README and Bulid UI with Logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballnumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              ballnumber = Random().nextInt(5) + 1;
+              print('Ball number: $ballnumber');
+            });
+          },
+          child: Image.asset('images/ball$ballnumber.png'),
+        ),
+      ),
+    );
+  }
+}
